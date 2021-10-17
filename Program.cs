1f9241b5e8ddac43b31cc868ca789305b4567942
Dell <Dell@ZISHAN>
2021-10-17 16:51:40 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AnonymousDelegatDemo
+{
+    class Program
+    {
+        //Declaring Delegates
+        public delegate void Delsum(int a, int b);
+        public delegate void DelSquare(int a);
+        static void Main(string[] args)
+        {
+            //Using Delegate Anonymously Without Refering to any Method
+            Delsum ds = delegate (int a, int b)
+            {
+                Console.WriteLine(a + b);
+            };
+            ds(22, 20);             //Passing Values
+            DelSquare ds1 = delegate (int a)
+              {
+                  Console.WriteLine(a * a);
+              };
+            ds1(4);
+        }
+    }
+}
